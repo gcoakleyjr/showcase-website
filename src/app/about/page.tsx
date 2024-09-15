d0b2121cc67be876d7fea867c1c585a2ed6863a2
Giovanni Coakley <95830133+gcoakleyjr@users.noreply.github.com>
2024-09-15 19:51:55 -0400

    updates

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -201,7 +201,7 @@ export default function About() {
                   <TextAnimation
                     hasScrolled={hasScrolled}
                     delayNumber={16}
-                    text="2021 - Present"
+                    text="2022 - Present"
                   />
                 </div>
               </li>
@@ -243,7 +243,7 @@ export default function About() {
                   <TextAnimation
                     hasScrolled={hasScrolled}
                     delayNumber={22}
-                    text="2018 - 2021"
+                    text="2018 - 2022"
                   />
                 </div>
               </li>
--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -22,7 +22,7 @@ export default function Resume() {
       <NavBar current="Resume" />
       <iframe
         style={{ position: "absolute", top: 100 }}
-        src="Giovanni_Resume_2024.pdf"
+        src="Giovanni_Resume_Resume.pdf"
         width={windowWidth}
         height="1127"
       ></iframe>
